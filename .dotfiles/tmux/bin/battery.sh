1d29629f92307b97699f1eb8e98ddc40f641e372
philodavies <philodavies@users.noreply.github.com>
2019-05-23 15:42:22 +0100

    Update tmux with colours for battery power, and removed space at end of statusline

--- .dotfiles/tmux/bin/battery.sh
+++ .dotfiles/tmux/bin/battery.sh
@@ -6,15 +6,15 @@ battery_info=`pmset -g batt`
 current_charge=$(echo $battery_info | grep -o '[0-9]\+%' | awk '{sub (/%/, "", $1); print $1}')
 
 if [[ $current_charge -lt 30 ]]; then
-    echo -n '#[fg=colour41]'
+    echo -n '#[fg=colour196]'
 elif [[ $current_charge -lt 50 ]]; then
-    echo -n '#[fg=colour42]'
+    echo -n '#[fg=colour214]'
 elif [[ $current_charge -lt 70 ]]; then
-    echo -n '#[fg=colour43]'
+    echo -n '#[fg=colour11]'
 elif [[ $current_charge -lt 90 ]]; then
-    echo -n '#[fg=colour44]'
+    echo -n '#[fg=colour48]'
 else
-    echo -n '#[fg=colour45]'
+    echo -n '#[fg=colour10]'
 fi
 
 echo -n "$HEART $current_charge"
